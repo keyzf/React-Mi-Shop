4fae39fa0c84363eb4ab22a506e5ccbdae8b7193
zhongxia245 <zhongxia245@sina.com>
2016-09-24 22:12:52 +0800

    init

---
+++ app/components/FooterNav/FooterNavItem.jsx
@@ -0,0 +1,45 @@
+/***************************************************
+ * 时间: 9/24/16 01:21
+ * 作者: zhongxia
+ * 说明:
+ *
+ ***************************************************/
+import React, { PropTypes } from 'react'
+import autoBind from 'react-autobind';
+import classnames from 'classnames'
+import Icon from '../Icon'
+
+class FooterNavItem extends React.Component {
+  constructor(prop) {
+    super(prop);
+    autoBind(this)
+  }
+
+  onActive() {
+    this.context.footerNav && this.context.footerNav.onActive(this.props.activeIndex);
+  }
+
+  render() {
+    const {type,title,active} = this.props;
+    return (
+      <li className={classnames("zx-nav__item",{"zx-nav__item-active":active})} onClick={this.onActive}>
+        <Icon type={type}/>
+        {title}
+      </li>
+    );
+  }
+}
+
+FooterNavItem.contextTypes = {
+  footerNav: PropTypes.object
+}
+
+FooterNavItem.propTypes = {
+  onActive: PropTypes.func,
+  active: PropTypes.bool
+}
+FooterNavItem.defaultProps = {
+  active: false
+}
+
+export default FooterNavItem
---
+++ app/components/Icon/index.jsx
@@ -0,0 +1,23 @@
+/***************************************************
+ * 时间: 9/24/16 01:18
+ * 作者: zhongxia
+ * 说明:
+ *
+ ***************************************************/
+import 'font-awesome/css/font-awesome.css'
+import React, {Component, PropTypes} from 'react';
+import classnames from 'classnames'
+
+
+const Icon = props => {
+  const {type, children,className,...other} = props;
+  return (
+    <i className={classnames("zx-icon","fa" ,`fa-${type}`,className)} aria-hidden="true" {...other}>{children}</i>
+  )
+}
+
+Icon.propTypes = {
+  type: PropTypes.string.isRequired
+};
+
+export default Icon;
